2574e65f2de3e44e0a7cbd1b0188d92101883ae0
ErikBoesen <me@erikboesen.com>
2018-10-29 21:43:09 -0400

    Clean up header creation code and name more accurately

--- src/wristcoach.c
+++ src/wristcoach.c
@@ -1,30 +1,27 @@
 #include <pebble.h>
 static Window *s_main_window;
-static TextLayer *s_time_layer;
+static TextLayer *s_header;
 
 static void main_window_load(Window *window) {
   // Get information about the Window
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  // Create the TextLayer with specific bounds
-  s_time_layer = text_layer_create(
+  // Set up header box
+  s_header = text_layer_create(
       GRect(0, 0, bounds.size.w, 20));
-
-  // Improve the layout to be more like a watchface
-  text_layer_set_background_color(s_time_layer, GColorBlack);
-  text_layer_set_text_color(s_time_layer, GColorRed);
-  text_layer_set_text(s_time_layer, "WristCoach");
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD));
-  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-
-  // Add it as a child layer to the Window's root layer
-  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+  text_layer_set_background_color(s_header, GColorBlack);
+  text_layer_set_text_color(s_header, GColorRed);
+  text_layer_set_text(s_header, "WristCoach by 1418");
+  text_layer_set_font(s_header, fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD));
+  text_layer_set_text_alignment(s_header, GTextAlignmentCenter);
+  // Insert into window
+  layer_add_child(window_layer, text_layer_get_layer(s_header));
 }
 
 static void main_window_unload(Window *window) {
   // Destroy TextLayer
-  text_layer_destroy(s_time_layer);
+  text_layer_destroy(s_header);
 }
 
 static void init() {
