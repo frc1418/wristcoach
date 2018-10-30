c992faf78d36d1faf9438e381313dc29cd38d167
ErikBoesen <me@erikboesen.com>
2018-10-29 21:21:26 -0400

    Add text layer

--- src/wristcoach.c
+++ src/wristcoach.c
@@ -1,12 +1,30 @@
 #include <pebble.h>
 static Window *s_main_window;
+static TextLayer *s_time_layer;
 
 static void main_window_load(Window *window) {
+  // Get information about the Window
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
 
+  // Create the TextLayer with specific bounds
+  s_time_layer = text_layer_create(
+      GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 100));
+
+  // Improve the layout to be more like a watchface
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorBlack);
+  text_layer_set_text(s_time_layer, "Drive coach reminder: don't jump around like a fool");
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
+  //text_layer_set_text_alignment(s_time_layer, GTextAlignmentLeft);
+
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 }
 
 static void main_window_unload(Window *window) {
-
+  // Destroy TextLayer
+  text_layer_destroy(s_time_layer);
 }
 
 static void init() {
