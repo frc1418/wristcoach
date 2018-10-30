06d1f22f752c74dec4bba2c06847e54fff93fafc
ErikBoesen <me@erikboesen.com>
2018-10-29 21:13:38 -0400

    Add empty window to app

--- src/wristcoach.c
+++ src/wristcoach.c
@@ -1,7 +1,26 @@
 #include <pebble.h>
+static Window *s_main_window;
+
+static void main_window_load(Window *window) {
+
+}
+
+static void main_window_unload(Window *window) {
+
+}
 
 static void init() {
+  // Create main Window element and assign to pointer
+  s_main_window = window_create();
 
+  // Set handlers to manage the elements inside the Window
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
+
+  // Show the Window on the watch, with animated=true
+  window_stack_push(s_main_window, true);
 }
 
 static void deinit() {
