8290a8e7df70917edbfbbff01352a8d390306bac
ErikBoesen <me@erikboesen.com>
2018-10-30 11:03:40 -0400

    Four-indent C code

--- src/wristcoach.c
+++ src/wristcoach.c
@@ -5,58 +5,56 @@ static TextLayer *s_timer;
 static TextLayer *s_message;
 
 static void main_window_load(Window *window) {
-  // Get information about the Window
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
+    // Get information about the Window
+    Layer *window_layer = window_get_root_layer(window);
+    GRect bounds = layer_get_bounds(window_layer);
 
-  // Set up header box
-  s_header = text_layer_create(GRect(0, 0, bounds.size.w, 30));
-  text_layer_set_background_color(s_header, GColorBlack);
-  text_layer_set_text_color(s_header, GColorRed);
-  text_layer_set_text(s_header, "WristCoach by 1418");
-  text_layer_set_font(s_header, fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD));
-  text_layer_set_text_alignment(s_header, GTextAlignmentCenter);
-  // Insert into window
-  layer_add_child(window_layer, text_layer_get_layer(s_header));
+    // Set up header box
+    s_header = text_layer_create(GRect(0, 0, bounds.size.w, 30));
+    text_layer_set_background_color(s_header, GColorBlack);
+    text_layer_set_text_color(s_header, GColorRed);
+    text_layer_set_text(s_header, "WristCoach by 1418");
+    text_layer_set_font(s_header, fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD));
+    text_layer_set_text_alignment(s_header, GTextAlignmentCenter);
+    // Insert into window
+    layer_add_child(window_layer, text_layer_get_layer(s_header));
 
-  // Set up timer box
-  s_timer = text_layer_create(GRect(0, 30, bounds.size.w, 80));
-  text_layer_set_background_color(s_timer, GColorBlack);
-  text_layer_set_text_color(s_timer, GColorRed);
-  text_layer_set_text(s_timer, "00:29");
-  text_layer_set_font(s_timer, fonts_get_system_font(FONT_KEY_BITHAM_42_LIGHT));
-  text_layer_set_text_alignment(s_timer, GTextAlignmentCenter);
-  // Insert into window
-  layer_add_child(window_layer, text_layer_get_layer(s_timer));
+    // Set up timer box
+    s_timer = text_layer_create(GRect(0, 30, bounds.size.w, 80));
+    text_layer_set_background_color(s_timer, GColorBlack);
+    text_layer_set_text_color(s_timer, GColorRed);
+    text_layer_set_text(s_timer, "00:29");
+    text_layer_set_font(s_timer, fonts_get_system_font(FONT_KEY_BITHAM_42_LIGHT));
+    text_layer_set_text_alignment(s_timer, GTextAlignmentCenter);
+    // Insert into window
+    layer_add_child(window_layer, text_layer_get_layer(s_timer));
 
-  // Set up message box
-  s_message = text_layer_create(GRect(0, 110, bounds.size.w, 60));
-  text_layer_set_background_color(s_message, GColorRed);
-  text_layer_set_text_color(s_message, GColorBlack);
-  text_layer_set_text(s_message, "Warning: It's time to climb!");
-  text_layer_set_font(s_message, fonts_get_system_font(FONT_KEY_GOTHIC_24));
-  text_layer_set_text_alignment(s_message, GTextAlignmentCenter);
-  // Insert into window
-  layer_add_child(window_layer, text_layer_get_layer(s_message));
+    // Set up message box
+    s_message = text_layer_create(GRect(0, 110, bounds.size.w, 60));
+    text_layer_set_background_color(s_message, GColorRed);
+    text_layer_set_text_color(s_message, GColorBlack);
+    text_layer_set_text(s_message, "Warning: It's time to climb!");
+    text_layer_set_font(s_message, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+    text_layer_set_text_alignment(s_message, GTextAlignmentCenter);
+    // Insert into window
+    layer_add_child(window_layer, text_layer_get_layer(s_message));
 }
 
 static void main_window_unload(Window *window) {
-  // Destroy TextLayer
-  text_layer_destroy(s_header);
+    // Destroy TextLayer
+    text_layer_destroy(s_header);
 }
+    // Create main Window element and assign to pointer
+    s_main_window = window_create();
 
-static void init() {
-  // Create main Window element and assign to pointer
-  s_main_window = window_create();
+    // Set handlers to manage the elements inside the Window
+    window_set_window_handlers(s_main_window, (WindowHandlers) {
+        .load = main_window_load,
+        .unload = main_window_unload
+    });
 
-  // Set handlers to manage the elements inside the Window
-  window_set_window_handlers(s_main_window, (WindowHandlers) {
-    .load = main_window_load,
-    .unload = main_window_unload
-  });
-
-  // Show the Window on the watch, with animated=true
-  window_stack_push(s_main_window, true);
+    // Show the Window on the watch, with animated=true
+    window_stack_push(s_main_window, true);
 }
 
 static void deinit() {
@@ -64,7 +62,7 @@ static void deinit() {
 }
 
 int main(void) {
-  init();
-  app_event_loop();
-  deinit();
+    init();
+    app_event_loop();
+    deinit();
 }
