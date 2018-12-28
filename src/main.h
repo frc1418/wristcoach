8420bb7d71560bd1c8481aaa63f98b67a0b7a914
ErikBoesen <me@erikboesen.com>
2018-12-27 23:37:29 -0500

    Add header file

---
+++ src/main.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <pebble.h>
+
+#define SETTINGS_KEY 1
+
+// A structure containing our settings
+typedef struct ClaySettings {
+  GColor BackgroundColor;
+  GColor ForegroundColor;
+  bool SecondTick;
+  bool Animations;
+} __attribute__((__packed__)) ClaySettings;
+
+// TODO: define functions here
